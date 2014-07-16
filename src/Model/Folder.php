8569ae5a5dfdc5f07a7aa3e8856e7bdc3b9ccd70
Grundik <grundik@ololo.cc>
2014-07-16 08:07:24 +0400

    Оптимизация: забываем про подкаталоги/файлы после сканирования

--- src/Model/Folder.php
+++ src/Model/Folder.php
@@ -164,6 +164,7 @@ class Folder extends Entry {
       foreach ($files as $file) {
         $em->detach($file);
       }
+      $this->files = null;
     }
     if ($this->_updateList($entries[1], $folders, $em)) {
       echo "...saving status".PHP_EOL;
@@ -171,6 +172,7 @@ class Folder extends Entry {
       foreach ($folders as $folder) {
         $em->detach($folder);
       }
+      $this->childs = null;
     }
     return false;
   }
